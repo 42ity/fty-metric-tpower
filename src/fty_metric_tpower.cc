5c992b0142ea6420d04c0745d8a0f96e52e11404
Jim Klimov <jim@jimklimov.com>
2017-03-21 22:09:57 +0100

    Problem: getopts short_option can be defined standalone

--- src/fty_metric_tpower.cc
+++ src/fty_metric_tpower.cc
@@ -66,6 +66,7 @@ int main (int argc, char *argv [])
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wwrite-strings"
 #endif
+    static const char *short_options = "hv";
     static struct option long_options[] =
     {
         {"help",       no_argument,       &help,    1},
@@ -78,7 +79,7 @@ int main (int argc, char *argv [])
 
     while(true) {
         int option_index = 0;
-        c = getopt_long (argc, argv, "hv", long_options, &option_index);
+        c = getopt_long (argc, argv, short_options, long_options, &option_index);
         if (c == -1) {
             break;
         }
