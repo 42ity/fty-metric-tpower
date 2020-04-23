f68b0c6c833efdf2b24816627c91795352fcb291
Jim Klimov <jim@jimklimov.com>
2020-04-23 11:54:08 +0200

    Bump copyrights to 2020

--- include/fty_metric_tpower.h
+++ include/fty_metric_tpower.h
@@ -1,7 +1,7 @@
 /*  =========================================================================
     agent-tpower - Computes power metrics
 
-    Copyright (C) 2014 - 2017 Eaton
+    Copyright (C) 2014 - 2020 Eaton
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
